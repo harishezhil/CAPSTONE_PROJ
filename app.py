593831f4577f6da6fbd0775821c154b40226e987
harish <harishezhil1431@gmail.com>
2025-02-20 22:40:26 +0530

    testing done

--- app.py
+++ app.py
@@ -4,16 +4,10 @@ from database import db
 from routes import register_routes
 
 app = Flask(__name__)
-app.config.from_object(Config)
-
-db.init_app(app)
-
-with app.app_context():
-    db.create_all()
-
-register_routes(app)
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
+def create_app():
+    app.config.from_object(Config)
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+    register_routes(app)
+    return app
